e39fec77ae7213889f693b27b60a2529da4ccda2
brucklin9-9 <a_aleckseev@mail.ru>
2022-08-05 14:45:59 +0300

    commit
    replay-1

--- echo_bot.py
+++ echo_bot.py
@@ -1,13 +1,16 @@
 import telebot
+from telebot import types
+import config
 
 bot = telebot.TeleBot("1663399217:AAHUbla9zvsaYbRIyihCWE-2uXHB3KH1dgs")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Вы сделали запрос старт или помощь.")
+@bot.message_handler(commands=['start'])
+def send_start(message):
+	markup = types.
+	bot.reply_to(message, "Привет, я твой помошник.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message.text)
+	bot.reply_to(message, message.text)
 
 bot.infinity_polling()
