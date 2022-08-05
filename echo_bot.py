f3b4355237bd789830d72b73429ce2e2df19ac7b
brucklin9-9 <a_aleckseev@mail.ru>
2022-08-05 14:01:09 +0300

    Update echo_bot.py
    Заменить.

--- echo_bot.py
+++ echo_bot.py
@@ -4,10 +4,10 @@ bot = telebot.TeleBot("1663399217:AAHUbla9zvsaYbRIyihCWE-2uXHB3KH1dgs")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Вы сделали запрос старт или помощь.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message.text)
 
 bot.infinity_polling()
