7853646528753075f9b48ffad37f0a0da64d0853
brucklin9-9 <a_aleckseev@mail.ru>
2022-09-20 00:07:12 +0300

    Update echo_bot.py
    20.09.2022

--- echo_bot.py
+++ echo_bot.py
@@ -1,12 +1,12 @@
 import telebot
 from telebot import types
-import config
+#import config
 
 bot = telebot.TeleBot("1663399217:AAHUbla9zvsaYbRIyihCWE-2uXHB3KH1dgs")
 
 @bot.message_handler(commands=['start'])
 def send_start(message):
-	markup = types.
+#	markup = types.
 	bot.reply_to(message, "Привет, я твой помошник.")
 
 @bot.message_handler(func=lambda message: True)
